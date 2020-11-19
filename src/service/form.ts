db98f725480ba7ba363b96b6f0d0bef280b69e1b
ISID Kumakura <isid_kumakura@KumakuranoMacBook-Pro.local>
2020-11-20 04:31:34 +0900

    sa

--- src/service/form.ts
+++ src/service/form.ts
@@ -5,14 +5,14 @@ export const handler = async (req: Express.Request) => {
   response.push({
     type: 'textarea',
     description: '担当営業へのメッセージを100文字以内で入力してください。',
-    name: 'to_member_message',
+    name: 'from_message',
     label: '担当営業へのメッセージ',
     required: true,
   });
   response.push({
     type: 'textarea',
     description: 'お客様へのメッセージテンプレートを75文字以内入力してください。',
-    name: 'to_customer_message',
+    name: 'to_message',
     label: 'お客様へのメッセージテンプレート',
     required: true,
   });
