b1476948572566cdec58e6dc43ab78283fd1a747
ISID Kumakura <isid_kumakura@KumakuranoMacBook-Pro.local>
2020-12-09 14:46:47 +0900

    sa

--- src/entity/lineworksAccessToken.ts
+++ src/entity/lineworksAccessToken.ts
@@ -15,9 +15,9 @@ export default class lineworksAccessToken {
   @Column()
   public expires_in: number = 0;
 
-  @CreateDateColumn({ name: 'created_at', type: 'timestamp', precision: 0, default: () => 'CURRENT_TIMESTAMP' })
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp', precision: 0 })
   public createdAt?: Date;
 
-  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp', precision: 0, default: () => 'CURRENT_TIMESTAMP' })
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp', precision: 0 })
   public updatedAt?: Date;
 }
